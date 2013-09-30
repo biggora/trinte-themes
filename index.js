86f1a18f43116cdad7a69941c4678c573a51c301
Alex <Alex@Alex-PC>
2013-09-30 11:43:13 +0300

    Add depend.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/** 
- * @project  trinte-themes
- * @author   trinte-themes
- * @created  2013-09-24 03:47:24
+/**
+ *  Twitter Bootstrap Themes
+ *
+ *  @project     TrinteJS
+ *  @version     0.0.1
+ *  @package     trinte-themes
+ *  @author      Aleksejs Gordejevs
+ *  @created     2013-09-24 03:47:24
  * 
- * 
- */
+ *  Created by init script
+ *  App based on TrinteJS MVC framework
+ *  TrinteJS homepage http://www.trintejs.com
+ **/
 
 module.exports = require('./lib/trinte-themes.js');
